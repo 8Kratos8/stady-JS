3a5aa83f32ba7348ed141c956e3e9369b64090b2
8Karatos8 <kvartlait@gmail.com>
2022-01-07 21:45:22 +0300

    Add new fail script.js

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+console.log(" Hallo world")
+let eisColor;
+let user;
+let userName;
+userName = "Vasya";
+user = userName;
+console.log(user);
+if (user === "Vasya"){
+    console.log(")))");
+}else {
+    console.log("(((");
+}
+let a = 2;
+let b = 5;
+let operator = 5 > 4 ? " vopros" :
+5 < 4 ?  "otvet" : "net otveta"
+console.log(operator);
