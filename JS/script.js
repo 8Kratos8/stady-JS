356e3bcf98f810d9304ae375965273f888033cdd
8Karatos8 <kvartlait@gmail.com>
2022-01-23 00:52:39 +0300

    added new task

--- JS/script.js
+++ JS/script.js
@@ -87,12 +87,86 @@ function freezeObj()
 let binNunber = fakeBin('454');
 console.log(binNunber);
 */
-let finalGrade = (exam, projects) => {
+// task complited
+/*const finalGrade = (exam, projects) => {
   return ( 
  exam > 90  || projects > 10 ? 100 :
  exam > 75 && projects >= 5 ? 90 :
  exam > 50 && projects >= 2 ? 75 : 0)
 }
                                    
+Exclamation marks series #2:  complited
 
-  console.log(finalGrade(76, 2));
+  console.log(finalGrade(73, 2)); */
+  /* is complited
+  function remove (string) {
+    let i = string.length - 1;
+      while (string[i] == "!") {
+        string = string.slice(0, -1);
+        i--;
+        };
+return string;
+  }
+console.log(remove("!hi!!!")) */
+//const remove = (s) => s[s.length - 1] !== '!' ? s : remove(s.slice(0, s.length - 1));
+  //console.log(remove("!!hi!!"));
+
+  // Rock Paper Scissors! complited
+  /*const rps = (p1, p2) => {
+      re
+    p1 == "scissors" && p2 == "paper" ? "Player 1 won!" :
+    p1 == "scissors" && p2 == "rock" ? "Player 2 won!" :
+    p1 == "rock" && p2 == "paper" ? "Player 2 won!" :
+    p1 == "rock" && p2 == "scissors" ? "Player 1 won!" : 
+    p1 == "paper" && p2 == "rock" ? "Player 1 won!" :
+    p1 == "paper" && p2 == "scissors" ? "Player 2 won!" : "Draw!"
+  }; */
+  //7 kyu
+//Driving School Series #2
+
+/*function cost (mins) { 
+    
+    let totalCost = 30;
+          while (mins > 65 ) {
+          mins -= 30;
+          totalCost += 10;
+    };
+      return totalCost;
+    } 
+console.log(cost(1200)); */
+/*let x = 100;
+let y = 10;
+function cost (count) { 
+  x = x - count;
+  y = y + count;
+}
+console.log(cost(80) );
+console.log("колич хсов" + x);
+  console.log ('колич уков' + y) */
+
+ /* function solution(nums){
+    nums.sort(function (a, b){
+      if (a > b) {
+        return 1;
+        };
+       if (a < b){
+         return -1;
+         }
+      if (nums === null || [""]){
+        return [""];
+        }
+    });
+      return nums;
+    } */
+    function sumOfIntegersInString(s){
+      var sum = 0;
+      let arr = s.match(/[0-9]+/g);
+      if (arr == null) return;
+      console.log(arr);
+     for (let i = 0; i < arr.length; i++){
+       sum = sum + Number(arr[i]);
+       console.log(arr[i]);
+     }
+     return sum ;
+   }
+   console.log(sumOfIntegersInString("Dogs are our best friends."))
